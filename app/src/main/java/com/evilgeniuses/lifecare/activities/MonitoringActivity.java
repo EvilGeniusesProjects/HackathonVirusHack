916c37334191e633aed3e24dc014e11c4780340e
UBERMENSCHALONE <62486782+UBERMENSCHALONE@users.noreply.github.com>
2020-05-04 19:27:15 +0500

    Разработка 3 вкладки

--- app/src/main/java/com/evilgeniuses/lifecare/activities/MonitoringActivity.java
+++ app/src/main/java/com/evilgeniuses/lifecare/activities/MonitoringActivity.java
@@ -2,15 +2,52 @@ package com.evilgeniuses.lifecare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.evilgeniuses.lifecare.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MonitoringActivity extends AppCompatActivity {
+public class MonitoringActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Intent intent;
+    String monitoringID;
+
+    ImageView imageViewBack;
+    FloatingActionButton floatingActionButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoring);
+        imageViewBack = findViewById(R.id.imageViewBack);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        imageViewBack.setOnClickListener(this);
+        floatingActionButton.setOnClickListener(this);
+
+        intent = getIntent();
+        monitoringID = intent.getStringExtra("monitoringID");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageViewBack:
+                finish();
+                break;
+
+            case R.id.buttonСreateDiary:
+                //writeNewUser();
+                Toast.makeText(getApplicationContext(), "text", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+
+            case  R.id.floatingActionButton:
+                break;
+        }
     }
 }
--- app/src/main/java/com/evilgeniuses/lifecare/models/Monitoring.java
+++ app/src/main/java/com/evilgeniuses/lifecare/models/Monitoring.java
@@ -1,13 +1,140 @@
 package com.evilgeniuses.lifecare.models;
 
-public class Monitoring {
-    private String Pressure;
-    private String Pulse;
-    private String Temperature;
-    private String Defecation;
-    private String Sleep;
-    private String SkinDamage;
-    private String Pain;
-    private String Stroll;
-    private String Leisure;
+public class Monitoring implements Comparable<Monitoring>{
+    public String ID;
+    public int Days;
+    public String Date;
+    public String Pressure;
+    public String Pulse;
+    public String Temperature;
+    public String Defecation;
+    public String Sleep;
+    public String SkinDamage;
+    public String Pain;
+    public String Stroll;
+    public String Leisure;
+
+    public Monitoring(String ID, int milliseconds, String date, String pressure, String pulse, String temperature, String defecation, String sleep, String skinDamage, String pain, String stroll, String leisure) {
+        this.ID = ID;
+        Days = milliseconds;
+        Date = date;
+        Pressure = pressure;
+        Pulse = pulse;
+        Temperature = temperature;
+        Defecation = defecation;
+        Sleep = sleep;
+        SkinDamage = skinDamage;
+        Pain = pain;
+        Stroll = stroll;
+        Leisure = leisure;
+    }
+
+    Monitoring(){
+
+    }
+
+    @Override
+    public int compareTo(Monitoring comparestu) {
+        int compareage=((Monitoring)comparestu).getDays();
+        return this.Days -  compareage;
+    }
+
+
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public int getDays() {
+        return Days;
+    }
+
+    public void setDays(int days) {
+        Days = days;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getPressure() {
+        return Pressure;
+    }
+
+    public void setPressure(String pressure) {
+        Pressure = pressure;
+    }
+
+    public String getPulse() {
+        return Pulse;
+    }
+
+    public void setPulse(String pulse) {
+        Pulse = pulse;
+    }
+
+    public String getTemperature() {
+        return Temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        Temperature = temperature;
+    }
+
+    public String getDefecation() {
+        return Defecation;
+    }
+
+    public void setDefecation(String defecation) {
+        Defecation = defecation;
+    }
+
+    public String getSleep() {
+        return Sleep;
+    }
+
+    public void setSleep(String sleep) {
+        Sleep = sleep;
+    }
+
+    public String getSkinDamage() {
+        return SkinDamage;
+    }
+
+    public void setSkinDamage(String skinDamage) {
+        SkinDamage = skinDamage;
+    }
+
+    public String getPain() {
+        return Pain;
+    }
+
+    public void setPain(String pain) {
+        Pain = pain;
+    }
+
+    public String getStroll() {
+        return Stroll;
+    }
+
+    public void setStroll(String stroll) {
+        Stroll = stroll;
+    }
+
+    public String getLeisure() {
+        return Leisure;
+    }
+
+    public void setLeisure(String leisure) {
+        Leisure = leisure;
+    }
+
 }
